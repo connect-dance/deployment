28a0cd0e8a6a47382e1115b61814b3ca81105e57
Marvin Strangfeld <marvin@strangfeld.io>
2024-05-04 17:59:41 +0200

    Use sops for dev secrets

---
+++ scripts/sops-encrypt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "${scriptDir}/.." || exit 1
+
+exec 3<<<"$(cat $1)"
+sops --encrypt --input-type json --output-type json /dev/fd/3
